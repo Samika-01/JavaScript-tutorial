3f26ddfce6179661df82a40c9b27216d093758b0
Samika-01 <samikataja01@gmail.com>
2025-08-27 20:04:09 +0545

    spread operator in js

---
+++ spread operator/minmax.js
@@ -0,0 +1,7 @@
+
+let text = "12345";
+let min = Math.min(...text);
+let max = Math.max(...text);
+
+document.getElementById("demo").innerHTML = 
+"Minimum val is: " +min + "<br>Maximum val is: " +max;
