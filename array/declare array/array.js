50018c58edf9643582d8778e1d7e1b41f6c1a109
Samika-01 <samikataja01@gmail.com>
2025-08-30 22:27:26 +0545

    array declaration

---
+++ array/declare array/array.js
@@ -0,0 +1,7 @@
+
+const cars = ["Saab", "Volvo", "Audi"];
+
+//let car = cars[cars.length-1];
+
+//document.getElementById("demo").innerHTML = (cars instanceof Array);
+document.getElementById("demo").innerHTML = (Array.isArray(cars));
