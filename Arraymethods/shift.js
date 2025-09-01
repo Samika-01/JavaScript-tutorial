8a5cfdee5616488d839e8c854164801af6ae74f6
Samika-01 <samikataja01@gmail.com>
2025-09-01 22:38:06 +0545

    add new methods

---
+++ Arraymethods/shift.js
@@ -0,0 +1,5 @@
+
+const fruits = ["Apple", "Banana", "Mango", "Orange"];
+let fruit = fruits.shift(); // removes the first array element and shift other to a lower index
+console.log(fruit);// removed element = Apple
+console.log(fruits);
---
+++ Arraymethods/unshift.js
@@ -0,0 +1,5 @@
+
+const fruits = ["Apple", "Banana", "Mango", "Orange"];
+let fruit = fruits.unshift("Kiwi");// add new element at the beginning
+console.log(fruit);// return the new length of array = 5
+console.log(fruits);// return the array with the added element
