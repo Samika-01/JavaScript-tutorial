f9f65ac1e9317b86569b52165d8396ce203e3ac2
Samika-01 <samikataja01@gmail.com>
2025-09-01 22:28:45 +0545

    add push method

---
+++ Arraymethods/push.js
@@ -0,0 +1,6 @@
+
+
+const fruits = ["Apple", "Banana", "Mango", "Orange"];
+let fruit = fruits.push("Kiwi"); // adds element at the end
+console.log(fruit);// returns new array length = 5
+console.log(fruits);
