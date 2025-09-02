353e5e67685dcc57bd84e5fe441032c00ae344a9
Samika-01 <samikataja01@gmail.com>
2025-09-02 22:14:05 +0545

    learn splice method

--- Arraymethods/splice.js
+++ Arraymethods/splice.js
@@ -1,3 +1,14 @@
 
 //The splice() method adds new items to an array.
-const fruits = ["Apple", "Banana", "Ki"]
+const fruits = ["Apple", "Banana", "Kiwi"];
+//let fruit = fruits.splice(2 ,0, "Mango");
+//The first parameter (2) defines the position where new elements should be added 
+//The second parameter (0) defines how many elements should be removed.
+//The rest of the parameters  define the new elements to be added
+
+//console.log(fruit);
+
+
+//to remove element
+let fruit = fruits.splice(0, 1);
+console.log(fruit);
